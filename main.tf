7af11730cff995f5eec14b8dbb196e37face6adb
KeenGWatanabe <mckeenweb@gmail.com>
2025-01-31 06:19:21 +0800

    y

--- main.tf
+++ main.tf
@@ -121,6 +121,24 @@ resource "aws_security_group" "roger_db_sg" {
   tags = {
     Name = "roger_db_sg"
   }
-
 }
-#stopped at Step6
+
+resource "aws_db_subnet_group" "roger_db_subnet_group" {
+ name = "roger_db_subnet_group" 
+ description = "db subnet group for roger"
+ subnet_ids = [for subnet in aws_subnet.roger_private_subnet : subnet.id]
+}
+
+resource "aws_db_instance" "roger_database" {
+  allocated_storage = var.settings.database.allocated_storage
+  engine = var.settings.database.engine
+  engine_version = var.settings.database.engine_version
+  instance_class = var.settings.instance_class
+  db_name = var.settings.database.db_name
+  username = var.db_username
+  password = var.db_password
+  db_subnet_group_name = aws_db_subnet_group.roger_db_subnet_group.id
+  vpc_security_group_ids = [aws_security_group.roger_db_sg.id]
+  skip_final_snapshot = var.settings.database.skip_final_snapshot
+}
+#stopped at Step7
