3797718cbcd640481739e5488878402b5599edaf
KeenGWatanabe <mckeenweb@gmail.com>
2025-01-31 21:48:56 +0800

    r

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {
   state = "available"
 }
-
+#1 vpc
 resource "aws_vpc" "roger_vpc" {
   cidr_block = var.vpc_cidr_block
   enable_dns_hostnames = true
@@ -23,14 +23,14 @@ resource "aws_vpc" "roger_vpc" {
     Name = "roger_vpc"
   }
 }
-
+#2 igw
 resource "aws_internet_gateway" "roger_igw" {
   vpc_id = aws_vpc.roger_vpc.id
   tags = {
     Name = "roger_igw"
   }
 }
-
+#3 public subnet
 resource "aws_subnet" "roger_public_subnet" {
   count = var.subnet_count.public
   vpc_id = aws_vpc.roger_vpc.id 
@@ -40,7 +40,7 @@ resource "aws_subnet" "roger_public_subnet" {
     Name = "roger_public_subnet_${count.index}"
   }
 }
-
+#private subnet
 resource "aws_subnet" "roger_private_subnet" {
   count = var.subnet_count.private
   vpc_id = aws_vpc.roger_vpc.id 
@@ -50,7 +50,7 @@ resource "aws_subnet" "roger_private_subnet" {
     Name = "roger_private_subnet_${count.index}"
   }
 }
-
+#4 public rtb
 resource "aws_route_table" "roger_public_rt" {
   vpc_id = aws_vpc.roger_vpc.id
   route {
@@ -65,7 +65,7 @@ resource "aws_route_table_association" "public" {
   subnet_id = aws_subnet.roger_public_subnet[count.index].id
 }
 
-
+#private rtb
 resource "aws_route_table" "roger_private_rt" {
   vpc_id = aws_vpc.roger_vpc.id
 }
@@ -75,7 +75,7 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.roger_private_rt.id
   subnet_id = aws_subnet.roger_private_subnet[count.index].id
 }
-
+#5 EC2 security grp
 resource "aws_security_group" "roger_web_sg" {
   name = "roger_web_sg"
   description = "security group for web servers"
@@ -106,7 +106,7 @@ resource "aws_security_group" "roger_web_sg" {
     Name = "roger_web_sg"
   }
 }
-
+#RDS security grp
 resource "aws_security_group" "roger_db_sg" {
   name = "roger_db_sg"
   description = "security group for DB"
@@ -122,13 +122,13 @@ resource "aws_security_group" "roger_db_sg" {
     Name = "roger_db_sg"
   }
 }
-
+#6 create DB subnet grp
 resource "aws_db_subnet_group" "roger_db_subnet_group" {
  name = "roger_db_subnet_group" 
  description = "db subnet group for roger"
  subnet_ids = [for subnet in aws_subnet.roger_private_subnet : subnet.id]
 }
-
+#7 create mySQL RDS
 resource "aws_db_instance" "roger_database" {
   allocated_storage = var.settings.database.allocated_storage
   engine = var.settings.database.engine
@@ -141,4 +141,42 @@ resource "aws_db_instance" "roger_database" {
   vpc_security_group_ids = [aws_security_group.roger_db_sg.id]
   skip_final_snapshot = var.settings.database.skip_final_snapshot
 }
-#stopped at Step7
+#8 create key-pair, not sure if necessary if already created!
+resource "aws_key_pair" "roger-debian-kp" {
+  key_name = "roger-debian-kp"
+  public_key = file("roger-debian-kp.pub") #public key of ssh
+}
+#create ubuntu ami
+data "aws_ami" "ubuntu" {
+  most_recent = "true"
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-sad/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+#create EC2 roger_web
+resource "aws_instance" "roger_web" {
+  count = var.settings.web_app.count
+  ami = data.aws_ami.ubuntu.id
+  instance_type = var.settings.web_app.instance_type
+  subnet_id = aws_subnet.roger_public_subnet[count.index].id
+  key_name = aws_key_pair.roger-debian-kp.key_name
+  vpc_security_group_ids = [aws_security_group.roger_web_sg.id]
+  tags ={
+    Name = "roger_web_${count.index}"
+  }
+}
+#create elastic IP for EC2
+resource "aws_eip" "roger_web_eip" {
+  count = var.settings.web_app.count
+  instance = aws_instance.roger_web[count.index].id
+  vpc = true
+  tags = {
+    Name = "roger_web_eip_${count.index}"
+  }
+}
